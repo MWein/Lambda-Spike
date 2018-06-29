5ee52d0bc281cf6b9210b2bc447764c03b543b65
Mike <mweinberg21@gmail.com>
2018-06-29 12:47:07 -0500

    Added eslint and other packages

--- src/goodbye.js
+++ src/goodbye.js
@@ -2,9 +2,9 @@ const goodbye = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: 'Goodbye',
-  };
+  }
 
-  callback(null, response);
+  callback(null, response)
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
--- src/hello.js
+++ src/hello.js
@@ -1,10 +1,12 @@
 const hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: 'hello',
-  };
+    body: {
+      message: 'hello',
+    },
+  }
 
-  callback(null, response);
+  callback(null, response)
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
