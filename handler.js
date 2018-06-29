75461e8aec6c770058e9152625b168503c10d1ef
Mike <mweinberg21@gmail.com>
2018-06-29 12:23:40 -0500

    Hello world

---
+++ handler.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: 'hello',
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+
+module.exports.goodbye = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: 'Goodbye',
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
